8d4b82d3cb6eea7441a9fb99adc8a957e300cfbb
lmodin <lisamodin@live.com>
2020-05-18 20:14:15 -0700

    Updating files after changing to Pseudoclassical and blinky Dancer spec tests passing

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,30 +5,35 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
     expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
+
   });
 
   it('should have a step function that makes its node blink', function() {
     sinon.spy(blinkyDancer.$node, 'toggle');
     blinkyDancer.step();
+console.log(blinkyDancer)
     expect(blinkyDancer.$node.toggle.called).to.be.true;
   });
 
   describe('dance', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
+      //console.log(blinkyDancer)
       expect(blinkyDancer.step.callCount).to.be.equal(0);
+
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
       expect(blinkyDancer.step.callCount).to.be.equal(1);
-
+      console.log(blinkyDancer)
       clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(2);
+      console.log(blinkyDancer.step.callCount)
     });
   });
 });
